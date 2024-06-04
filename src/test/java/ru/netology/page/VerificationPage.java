853e1737ffce0475c62a613b6b6870c2cebca6cc
Tatyanochka <tanyasmit16@yandex.ru>
2024-06-04 06:30:31 +0300

    fourth commit

--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Selenide.$;
 public class VerificationPage {
     private SelenideElement codeField = $("[data-test-id=code] input");
     private SelenideElement verifyButton = $("[data-test-id=action-verify]");
-    private SelenideElement errorNotification = $("[data-test-id=error-notification] .notification__content");
+    private SelenideElement errorNotification = $("[data-test-id='error-notification'] .notification__content");
 
     public void verificationPageVisibility() {
         codeField.shouldBe(visible);
